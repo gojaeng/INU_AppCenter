7635a6f563fcc0fba79b832945e030aa72c24366
고재현 <gojh1013@gmail.com>
2023-10-02 00:01:37 +0900

    앱센터 2주차 과제(2)

--- study/src/ChatList.tsx
+++ study/src/ChatList.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const ChatList: React.FC<{ chatItems: { title: string; chatId: string; lastChat: number }[] }> = ({ chatItems }) => {
-    const today = new Date().getTime();
+    const lastDay = new Date().getTime();
 
     const groupByDate: {
-        today?: { title: string; chatId: string; lastChat: number }[];
+        lastDay?: { title: string; chatId: string; lastChat: number }[];
         lastWeek?: { title: string; chatId: string; lastChat: number }[];
         lastMonth?: { title: string; chatId: string; lastChat: number }[];
     } = {};
 
     chatItems.forEach((item) => {
-        const diff = Math.floor((today - item.lastChat) / (1000 * 3600 * 24));
-        const groupKey = diff <= 1 ? 'today' : diff <= 7 ? 'lastWeek' : 'lastMonth';
+        const diff = Math.floor((lastDay - item.lastChat) / (1000 * 3600 * 24));
+        const groupKey = diff <= 1 ? 'lastDay' : diff <= 7 ? 'lastWeek' : 'lastMonth';
 
         groupByDate[groupKey] = [];
         groupByDate[groupKey]?.push(item);
@@ -19,11 +19,11 @@ const ChatList: React.FC<{ chatItems: { title: string; chatId: string; lastChat:
 
     return (
         <div>
-            {groupByDate.today && (
+            {groupByDate.lastDay && (
                 <>
                     <h2>지난 1일</h2>
 
-                    {groupByDate.today.map((chat) => (
+                    {groupByDate.lastDay.map((chat) => (
                         <li key={chat.chatId}>{chat.title}</li>
                     ))}
                 </>
