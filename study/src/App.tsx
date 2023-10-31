d7389e6641f555eca049d3d4434554b3ba16d3a2
고재현 <gojh1013@gmail.com>
2023-10-31 18:50:04 +0900

    Test : Check Resources

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,37 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ChatList from './ChatList';
+import { Icons } from './Icons';
+import styled from 'styled-components';
+
+const Side = styled.div`
+    width: 23vh;
+    height: 100vh;
+    background-color: #202123;
+    color: #ececf1;
+`;
+const NewChat = styled.button`
+    width: 150px;
+    height: 50px;
+`;
 
 function App() {
-    const chatItems = [
-        {
-            title: 'react에서 list 렌더링 하는 법',
-            chatId: '3',
-            lastChat: new Date().getTime(),
-        },
-        {
-            title: 'react 장점',
-            chatId: '2',
-            lastChat: new Date().getTime() - 3600 * 24 * 3 * 1000,
-        },
-        {
-            title: 'react JSX',
-            chatId: '1',
-            lastChat: new Date().getTime() - 3600 * 24 * 8 * 1000,
-        },
-        {
-            title: 'react 프레임워크',
-            chatId: '0',
-            lastChat: new Date().getTime() - 3600 * 24 * 9 * 1000,
-        },
-    ];
-
+    const Image = '/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg';
+    const setting = Icons.setting;
+    const sidebar = Icons.sidebar;
+    const add = Icons.add;
+    const chat = Icons.chat;
+    const logout = Icons.logout;
+    const more = Icons.more;
     return (
-        <div className="App">
-            <h1>채팅</h1>
-            <ChatList chatItems={chatItems} />
-        </div>
+        <Side>
+            <NewChat>{add}</NewChat>
+            <img src={Image} alt="Profile" style={{ width: '30px' }} />
+            안녕하세요
+        </Side>
     );
 }
 
