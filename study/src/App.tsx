5a300fcb5b6bd7b1d65f5ada5cb62613aab6ff7a
고재현 <gojh1013@gmail.com>
2023-11-01 10:10:38 +0900

    Restore : ChatList

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import { Icons } from './Icons';
 import styled from 'styled-components';
+import ChatList from './ChatList';
 
 const Side = styled.div`
     width: 23vh;
     height: 100vh;
     background-color: #202123;
     color: #ececf1;
+    border-color: rgba(255, 255, 255, 0.2);
 `;
 const NewChat = styled.button`
     width: 150px;
     height: 50px;
+    background-color: #202123;
+    color: #ececf1;
+    border-color: rgba(255, 255, 255, 0.2);
+    border-radius: 10px;
 `;
 
 function App() {
@@ -23,12 +29,42 @@ function App() {
     const chat = Icons.chat;
     const logout = Icons.logout;
     const more = Icons.more;
+    const chatItems = [
+        {
+            title: 'react에서 list 렌더링 하는 법',
+            chatId: '3',
+            lastChat: new Date().getTime(),
+        },
+        {
+            title: 'react 장점',
+            chatId: '2',
+            lastChat: new Date().getTime() - 3600 * 24 * 3 * 1000,
+        },
+        {
+            title: 'react JSX',
+            chatId: '1',
+            lastChat: new Date().getTime() - 3600 * 24 * 8 * 1000,
+        },
+        {
+            title: 'react 프레임워크',
+            chatId: '0',
+            lastChat: new Date().getTime() - 3600 * 24 * 9 * 1000,
+        },
+    ];
+
     return (
-        <Side>
-            <NewChat>{add}</NewChat>
-            <img src={Image} alt="Profile" style={{ width: '30px' }} />
-            안녕하세요
-        </Side>
+        <>
+            <div className="App">
+                <ChatList chatItems={chatItems} />
+            </div>
+            <Side>
+                <h3>채팅</h3>
+                <NewChat>{add}</NewChat>
+                <br></br>
+                <img src={Image} alt="Profile" style={{ width: '30px' }} />
+                안녕하세요
+            </Side>
+        </>
     );
 }
 
